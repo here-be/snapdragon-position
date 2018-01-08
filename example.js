6da5917a2415e8047a2258c248fbc6283bd6f67a
jonschlinkert <jon.schlinkert@sellside.com>
2018-01-08 01:43:21 -0500

    adds example

---
+++ example.js
@@ -0,0 +1,10 @@
+const position = require('./');
+const Lexer = require('snapdragon-lexer');
+const lexer = new Lexer('foo/bar');
+lexer.use(position('loc'));
+
+lexer.capture('slash', /^\//);
+lexer.capture('text', /^\w+/);
+
+var token = lexer.advance();
+console.log(token);
